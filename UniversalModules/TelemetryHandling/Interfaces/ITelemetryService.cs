55a4777e6f90c838970c1511cd68f17e48d0a55b
Nikita Ponomarenko <nikita@teknikacorp.com>
2024-05-11 15:09:31 +0300

    - Forgot to add methods to the ITelemetryService interface;

--- UniversalModules/TelemetryHandling/Interfaces/ITelemetryService.cs
+++ UniversalModules/TelemetryHandling/Interfaces/ITelemetryService.cs
@@ -2,5 +2,7 @@
 
 public interface ITelemetryService
 {
+    void TrackException(Exception exception, IDictionary<string, string>? properties = null, IDictionary<string, double>? metrics = null);
 
+    void TrackEvent(string eventName, IDictionary<string, string>? properties = null, IDictionary<string, double>? metrics = null);
 }

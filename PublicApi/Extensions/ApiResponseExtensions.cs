8e556c7aea56da3fcd6f2125e58e9dbf676d910b
Nikita Ponomarenko <nikita@teknikacorp.com>
2024-05-28 17:30:00 +0300

    - Bumped srcs; - New classes/DTOs and utility extensions;

---
+++ PublicApi/Extensions/ApiResponseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Results;
+using snowcoreBlog.PublicApi.Utilities.Api;
+
+namespace snowcoreBlog.PublicApi;
+
+public static class ApiResponseExtensions
+{
+    public static ApiResponse ToApiResponse<T>(this IResult<T> result, int dataCount = 0) where T : notnull
+    {
+        if (result is SuccessResult<T> success)
+        {
+            return new(success.Data.ToJsonElement(), dataCount, 0);
+        }
+        else if (result is IErrorResult<T> error)
+        {
+            return new(default, 0, -1, error.Errors.Select(x => $"{x.Code}: {x.Details}").ToList());
+        }
+        else
+        {
+            return new(default, 0, -1, [ApiResponseConstants.UnknownError]);
+        }
+    }
+}
---
+++ PublicApi/Validation/Dto/CreateReaderAccountValidation.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace snowcoreBlog.PublicApi;
+
+public class CreateReaderAccountValidation : AbstractValidator<CreateReaderAccountDto>
+{
+    public CreateReaderAccountValidation()
+    {
+        RuleFor(x => x.NickName).NotEmpty();
+    }
+}

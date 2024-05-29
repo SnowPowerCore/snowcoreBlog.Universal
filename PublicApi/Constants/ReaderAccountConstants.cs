35f94c62ca3bbf35ad0402580a4d9ab464d025f1
SnowPowerCore <superprojects@live.ru>
2024-05-30 00:29:40 +0300

    - And consts;

--- PublicApi/Constants/ReaderAccountConstants.cs
+++ PublicApi/Constants/ReaderAccountConstants.cs
@@ -4,4 +4,6 @@ public class ReaderAccountConstants
 {
     public const string CreateReaderAccountResult = nameof(CreateReaderAccountResult);
     public const string CreateUserForReaderAccountResult = nameof(CreateUserForReaderAccountResult);
+    public const string UserForReaderAccountCreationGenericError = nameof(UserForReaderAccountCreationGenericError);
+    public const string ReaderAccountUnableToCreateUpdateError = nameof(ReaderAccountUnableToCreateUpdateError);
 }

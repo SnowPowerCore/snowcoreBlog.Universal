fd0e5cf10e354c9f5b175ebb577923865dcc765a
SnowPowerCore <superprojects@live.ru>
2024-06-01 01:55:47 +0300

    - Changes related to the latest testing;

--- PublicApi/Utilities/Api/ApiResponse.cs
+++ PublicApi/Utilities/Api/ApiResponse.cs
@@ -2,4 +2,4 @@
 
 namespace snowcoreBlog.PublicApi.Utilities.Api;
 
-public record ApiResponse(JsonElement? Data = default, int DataCount = 0, int StatusCode = -1, IReadOnlyCollection<string>? Errors = default);
+public sealed record ApiResponse(JsonElement? Data = default, int DataCount = 0, int StatusCode = -1, IReadOnlyCollection<string>? Errors = default);
---
+++ PublicApi/Utilities/DataResult/DataResult.cs
@@ -0,0 +1,12 @@
+﻿using Results;
+
+namespace snowcoreBlog.PublicApi;
+
+public sealed record DataResult<TValue>(TValue? Value = default, List<ErrorResultDetail> Errors = default)
+{
+    public bool IsSuccess => Errors.Count == 0;
+
+    public bool IsFailed => Errors.Count != 0;
+
+    public ErrorResultDetail? Error => Errors.FirstOrDefault();
+}

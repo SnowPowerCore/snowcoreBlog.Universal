d2b78e22eaa78184d61a043ab9282f9b3b764c0e
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-10-17 11:10:16 +0300

    - Changed DataResult a bit;

--- PublicApi/Utilities/DataResult/DataResult.cs
+++ PublicApi/Utilities/DataResult/DataResult.cs
@@ -4,9 +4,9 @@ namespace snowcoreBlog.PublicApi.Utilities.DataResult;
 
 public sealed record DataResult<TValue>(TValue? Value = default, List<ErrorResultDetail> Errors = default)
 {
-    public bool IsSuccess => Errors.Count == 0;
+    public bool IsSuccess => Errors is default(List<ErrorResultDetail>);
 
-    public bool IsFailed => Errors.Count != 0;
+    public bool IsFailed => Errors is not default(List<ErrorResultDetail>) && Errors.Count != 0;
 
-    public ErrorResultDetail? Error => Errors.FirstOrDefault();
+    public ErrorResultDetail? Error => Errors?.FirstOrDefault();
 }

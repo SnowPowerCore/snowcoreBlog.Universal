7435da7d638189183ef80294fcc731275053c617
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-11-18 00:07:27 +0300

    - Upd;

--- PublicApi/Extensions/ApiResponseExtensions.cs
+++ PublicApi/Extensions/ApiResponseExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Collections;
+using System.Text.Json;
 using Results;
 using snowcoreBlog.PublicApi.Constants;
 using snowcoreBlog.PublicApi.Utilities.Api;
@@ -7,10 +8,11 @@ namespace snowcoreBlog.PublicApi.Extensions;
 
 public static class ApiResponseExtensions
 {
-    public static ApiResponse ToApiResponse<T>(this IResult<T> result, int dataCount = 0, JsonSerializerOptions serializerOptions = null) where T : notnull
+    public static ApiResponse ToApiResponse<T>(this IResult<T> result, JsonSerializerOptions serializerOptions = null) where T : notnull
     {
         if (result is SuccessResult<T> success)
         {
+            var dataCount = success.Data is ICollection e ? e.Count : 1;
             return new(success.Data.ToJsonElement(serializerOptions), dataCount, 0);
         }
         else if (result is IErrorResult<T> error)

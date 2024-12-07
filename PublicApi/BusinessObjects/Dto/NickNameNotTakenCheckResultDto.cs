dc78c6ae6f39a18d977e0cc3406d1ab25317fe14
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-12-08 01:18:17 +0300

    - Explicit check nickname endpoint;

---
+++ PublicApi/BusinessObjects/Dto/NickNameNotTakenCheckResultDto.cs
@@ -0,0 +1,3 @@
+﻿namespace snowcoreBlog.PublicApi.BusinessObjects.Dto;
+
+public sealed record NickNameNotTakenCheckResultDto(bool WasTaken);
---
+++ PublicApi/Validation/Dto/CheckNickNameNotTakenValidation.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using snowcoreBlog.PublicApi.BusinessObjects.Dto;
+
+namespace snowcoreBlog.PublicApi.Validation.Dto;
+
+public sealed class CheckNickNameNotTakenValidation : AbstractValidator<CheckNickNameNotTakenDto>
+{
+    public CheckNickNameNotTakenValidation()
+    {
+        RuleFor(x => x.NickName).NotEmpty().Length(3, 30);
+    }
+}

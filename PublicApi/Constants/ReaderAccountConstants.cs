2f41612c747a6aa2ef724213143037fcecf2cc29
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-12-15 22:54:46 +0300

    - Upd;

--- PublicApi/Constants/ReaderAccountConstants.cs
+++ PublicApi/Constants/ReaderAccountConstants.cs
@@ -2,6 +2,7 @@
 
 public sealed class ReaderAccountConstants
 {
+    public const string CreateReaderAccountUserResult = nameof(CreateReaderAccountUserResult);
     public const string CreateReaderAccountResult = nameof(CreateReaderAccountResult);
     public const string ReaderAccountUnableToCreateUpdateError = nameof(ReaderAccountUnableToCreateUpdateError);
     public const string ReaderAccountAlreadyExistsError = nameof(ReaderAccountAlreadyExistsError);

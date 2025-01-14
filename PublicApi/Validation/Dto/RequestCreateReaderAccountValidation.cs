a79eeb6328f85e466077d7f5a42dd3f74ba3d4c6
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-01-14 17:53:00 +0300

    - Upd;

--- PublicApi/Validation/Dto/RequestCreateReaderAccountValidation.cs
+++ PublicApi/Validation/Dto/RequestCreateReaderAccountValidation.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using snowcoreBlog.PublicApi.BusinessObjects.Dto;
+using snowcoreBlog.PublicApi.Extensions;
 
 namespace snowcoreBlog.PublicApi.Validation.Dto;
 
@@ -10,6 +11,7 @@ public sealed class RequestCreateReaderAccountValidation : AbstractValidator<Req
         RuleFor(x => x.NickName).NotEmpty().Length(3, 30);
         RuleFor(x => x.Email).EmailAddress().MinimumLength(3);
         RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.PhoneNumber).PhoneShared().When(x => !string.IsNullOrEmpty(x.PhoneNumber));
         RuleFor(x => x.ConfirmedAgreement).Equal(true);
         RuleFor(x => x.InitialEmailConsent).Equal(true);
     }

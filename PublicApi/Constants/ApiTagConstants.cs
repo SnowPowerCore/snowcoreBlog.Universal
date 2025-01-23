9cf3c50e6ad860d226b4a8d64f9c0a3c3b576dda
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-01-24 02:08:37 +0300

    - refitter;

---
+++ PublicApi/Constants/ApiTagConstants.cs
@@ -0,0 +1,7 @@
+namespace snowcoreBlog.PublicApi.Constants;
+
+public sealed class ApiTagConstants
+{
+    public const string Tokens = nameof(Tokens);
+    public const string ReaderAccountManagement = nameof(ReaderAccountManagement);
+}

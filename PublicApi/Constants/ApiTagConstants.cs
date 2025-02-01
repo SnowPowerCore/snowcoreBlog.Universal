87500b96e990cf00c39e963288a1d30b567ac952
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-01 20:15:57 +0300

    - Upd;

--- PublicApi/Constants/ApiTagConstants.cs
+++ PublicApi/Constants/ApiTagConstants.cs
@@ -3,6 +3,7 @@ namespace snowcoreBlog.PublicApi.Constants;
 public sealed class ApiTagConstants
 {
     public const string Tokens = nameof(Tokens);
+    public const string AntiforgeryGeneration = nameof(AntiforgeryGeneration);
     public const string Captcha = nameof(Captcha);
     public const string ReaderAccountManagement = nameof(ReaderAccountManagement);
 }
--- PublicApi/Constants/HeaderKeyConstants.cs
+++ PublicApi/Constants/HeaderKeyConstants.cs
@@ -2,5 +2,6 @@
 
 public static class HeaderKeyConstants
 {
-    public const string CaptchaHeader = "Captcha";
+    public const string CaptchaHeader = "RequestCaptcha";
+    public const string AntiforgeryHeader = "RequestVerificationToken";
 }

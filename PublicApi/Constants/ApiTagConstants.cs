e78d5080b8843116255334d4d3d0db0bb53c5b36
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-01 20:55:46 +0300

    - Removed;

--- PublicApi/Constants/ApiTagConstants.cs
+++ PublicApi/Constants/ApiTagConstants.cs
@@ -3,7 +3,6 @@ namespace snowcoreBlog.PublicApi.Constants;
 public sealed class ApiTagConstants
 {
     public const string Tokens = nameof(Tokens);
-    public const string AntiforgeryGeneration = nameof(AntiforgeryGeneration);
     public const string Captcha = nameof(Captcha);
     public const string ReaderAccountManagement = nameof(ReaderAccountManagement);
 }
